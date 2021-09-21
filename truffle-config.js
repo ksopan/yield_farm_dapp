addf21bd926ad5b95889f938714a84c38d8f8705
ksopan <90927149+ksopan@users.noreply.github.com>
2021-09-21 13:57:52 -0600

    Initial set up

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+// set up ganache here
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+    networks:{
+        development:{
+            host:'127.0.0.1:',
+            port:'7545',
+            network_id:'*' // match to any network
+        },
+    },
+    contracts_directory:'./src/contracts/',
+    contracts_build_directory:'./src/truffle_abis/',
+    compilers: {
+        solc: {
+            version: '^0.5.0',
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+        },
+    },
+}
