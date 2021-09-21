a99e6a30dececbda7b3da476cfbfee47d3253b07
sopank <sopank@Brahma.hitronhub.home>
2021-09-21 15:07:00 -0600

    migration

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -3,4 +3,22 @@ pragma solidity '^0.5.0';
 contract Migrations {
     address public owner;
     uint public last_completed_migrations;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier restricted(){
+        if (msg.sender == owner) _; // if true continue
+    }
+
+    function set_completed(uint completed) public restricted {
+        last_completed_migrations = completed;
+    }
+
+    function upgrade(uint new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.set_completed(last_completed_migrations);
+    }
+
 }
