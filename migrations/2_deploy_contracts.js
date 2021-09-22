9174415acd0c4720b85fc43587bda30cfdac0bce
sopank <sopank@Brahma.hitronhub.home>
2021-09-22 03:11:39 -0600

    added reward token and decentralised bank

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
 module.exports = async function(deployer) {
+
+// Deploy tether contract
     await deployer.deploy(Tether)
+
+// Deploy RWD contract
+    await deployer.deploy(RWD)
+
+// Deploy DecentralBank contract
+    await deployer.deploy(DecentralBank)
 };
